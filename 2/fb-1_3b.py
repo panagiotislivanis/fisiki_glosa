569099cc50622c1497b2e7c6128a4fe25854949f
panagiotis livanis <panagiotislivanis@gmail.com>
2023-06-07 17:40:23 +0300

    pros_ergasia

--- 2/fb-1_3b.py
+++ 2/fb-1_3b.py
@@ -4,11 +4,11 @@ from statsmodels.tsa.arima.model import ARIMA
 
 df = pd.read_csv('Earthquakes_v2.csv')
 
-# Ensure the timestamp is in datetime format and set it as the index
+
 df['DATETIME'] = pd.to_datetime(df['DATETIME'])
 df.set_index('DATETIME', inplace=True)
 
-# Filter data for the last two years
+
 current_year = pd.Timestamp.now().year
 start_date = pd.Timestamp(year=current_year-1, month=1, day=1)
 end_date = pd.Timestamp(year=current_year, month=12, day=31)
